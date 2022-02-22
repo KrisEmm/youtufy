eb5a334f3afeed6d214f07f5c04fe25f23a1c02b
krisemm <kristian.emm@gmail.com>
2022-02-21 19:33:28 -0600

    fix: youtubeVideos.routes.ts modified

--- backend/platform/src/app/rest/routes/youtubeVideos.routes.ts
+++ backend/platform/src/app/rest/routes/youtubeVideos.routes.ts
@@ -1,7 +1,8 @@
 import express, { Router } from 'express';
+import { YoutubeVideosGetController } from 'krisemm/app/rest/controllers/youtubeVideos/YoutubeVideosGetController';
 import { container } from 'krisemm/app/rest/services';
 
-export const router:Router = express.Router();
-const youtubeVideosGetController = container.get('App.Rest.Controllers.YoutubeVideosGetController');
+export const router: Router = express.Router();
+const youtubeVideosGetController: YoutubeVideosGetController = container.get('App.Rest.Controllers.YoutubeVideosGetController');
 
-router.get('youtube/videos',youtubeVideosGetController.execute.bind(youtubeVideosGetController))
+router.get('/youtube/videos', youtubeVideosGetController.execute.bind(youtubeVideosGetController));
