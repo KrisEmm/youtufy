d9c916a5d9c95f3f599e8c98171eb2e156618572
krisemm <kristian.emm@gmail.com>
2022-02-22 23:26:44 -0600

    fix: VideoToConvertLaterPutController type added

--- backend/platform/src/app/rest/routes/videoToConvertLater.routes.ts
+++ backend/platform/src/app/rest/routes/videoToConvertLater.routes.ts
@@ -1,8 +1,9 @@
 import express, { Router } from 'express';
+import { VideoToConvertLaterPutController } from 'krisemm/app/rest/controllers/videoToConvertLater/VideoToConvertLaterPutController';
 import { container } from 'krisemm/app/rest/services';
 
 export const router:Router = express.Router()
 
-const videoToConvertLaterPutController = container.get('App.Rest.Controllers.VideoToConvertLaterPutController')
+const videoToConvertLaterPutController:VideoToConvertLaterPutController = container.get('App.Rest.Controllers.VideoToConvertLaterPutController')
 
 router.put('/videos/:id', videoToConvertLaterPutController.execute.bind(videoToConvertLaterPutController))
