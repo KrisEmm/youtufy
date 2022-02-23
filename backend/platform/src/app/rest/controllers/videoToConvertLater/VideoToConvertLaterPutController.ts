7e6a9069bbfaaf1a49253f71609a19ceb3a636b0
krisemm <kristian.emm@gmail.com>
2022-02-22 23:26:08 -0600

    feat: VideoToConvertLaterCreator added into VideoToConvertLaterPutController and updated

--- backend/platform/src/app/rest/controllers/videoToConvertLater/VideoToConvertLaterPutController.ts
+++ backend/platform/src/app/rest/controllers/videoToConvertLater/VideoToConvertLaterPutController.ts
@@ -1,13 +1,45 @@
 import { NextFunction, Request, Response } from 'express';
 import { RestController } from 'krisemm/context/shared/infrastructure/express/controllers/RestController';
+import { VideoToConvertLaterCreator } from 'krisemm/context/videoToConvertLater/application/create/VideoToConvertLaterCreator';
+import { VideoToConvertLaterRequest } from 'krisemm/context/videoToConvertLater/application/create/VideoToConvertLaterRequest';
 
 export class VideoToConvertLaterPutController extends RestController {
 
+  private readonly videoToConvertLaterCreator: VideoToConvertLaterCreator;
+
+  constructor(videoToConvertLaterCreator: VideoToConvertLaterCreator) {
+    super();
+    this.videoToConvertLaterCreator = videoToConvertLaterCreator;
+  }
+
   async execute(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
-      await res.status(201).send()
+      const { id } = req.params;
+      const { idYoutubeVideo, title, description, thumbnail, channel } = req.body;
+      const videoToConvertLaterRequest = new VideoToConvertLaterRequest(
+        id,
+        idYoutubeVideo,
+        title,
+        description,
+        thumbnail,
+        channel
+        );
+
+      await this.videoToConvertLaterCreator.create(
+        videoToConvertLaterRequest.id,
+        videoToConvertLaterRequest.idYoutubeVideo,
+        videoToConvertLaterRequest.title,
+        videoToConvertLaterRequest.description,
+        videoToConvertLaterRequest.thumbnail,
+        videoToConvertLaterRequest.channel
+      )
+
+      res.status(201).send();
+
     } catch (e) {
+
       this.handleError(<Error>e, req, next);
+
     }
   }
 
