5776e6206dfb119f1a7324a875a69f37b80c650a
krisemm <kristian.emm@gmail.com>
2022-02-22 20:02:23 -0600

    feat: VideosToConvertLaterPutController added

---
+++ backend/platform/src/app/rest/controllers/videosToConvertLater/VideosToConvertLaterPutController.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response } from 'express';
+import { RestController } from 'krisemm/context/shared/infrastructure/express/controllers/RestController';
+
+export class VideosToConvertLaterPutController extends RestController {
+
+  async execute(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      await res.status(201).send()
+    } catch (e) {
+      this.handleError(<Error>e, req, next);
+    }
+  }
+
+  exceptionCodeMapping(): Map<string, number> {
+    return new Map<string, number>([]);
+  }
+}
